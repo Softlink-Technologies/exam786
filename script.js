71a1330127186dc722b649600dceb08fdbf01dbd
SACHIN <119405675+sachin-fullstack@users.noreply.github.com>
2023-03-09 22:15:30 +0530

    Update script.js
    Signed-off-by: SACHIN <119405675+sachin-fullstack@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-// Store the user's name in a variable
-let name = "";
+// Set the time limit for the exam (in minutes)
+const timeLimit = 5;
 
-// Add an event listener to the form submit button
-document.querySelector('form').addEventListener('submit', function(e) {
-    // Prevent the default form submission behavior
-    e.preventDefault();
+// Get the form element and add an event listener for form submission
+const form = document.getElementById("exam-form");
+form.addEventListener("submit", submitExam);
 
-    // Get the user's name from the input field
-    name = document.querySelector('#name').value;
+// Get the countdown element and set the initial countdown value
+const countdownElement = document.getElementById("countdown");
+let countdown = timeLimit * 60;
 
-    // Redirect the user to the exam page
-    window.location.href = 'exam.html';
-});
+// Start the countdown timer
+setInterval(() => {
+    countdown--;
+    updateCountdown();
+    if (countdown === 0) {
+        submitExam();
+    }
+}, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(countdown / 60);
+    const seconds = countdown % 60;
+    countdownElement.innerText = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+}
+
+function submitExam(event) {
+    if (event) {
+        event.preventDefault
